ca7e7512e27f921759f780ea69dd01055803b8b8
Lucas Werner <lwerner27@gmail.com>
2024-04-11 18:35:04 +0000

    Created the basic structure for the home page.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
   import "../app.css";
 </script>
 
+<div class="navbar bg-neutral">
+  <div class="container mx-auto">
+    <a href="/" class="btn btn-ghost text-xl">Jamf School Tools</a>
+  </div>
+</div>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
+<script>
+  import ToolCard from "../components/ToolCard.svelte";
+</script>
+
+<div class="container mx-auto">
+  <div class="flex flex-row justify-around mt-10">
+    <ToolCard
+      cardTitle="Jamf Connect Wizard"
+      cardColor="bg-accent"
+      btnColor="btn-neutral"
+      btnLink="/jc-wizard"
+    >
+      A tool for making it easier to generate Configuration Profiles for Jamf
+      Connect Login and Jamf Connect Menubar.
+    </ToolCard>
+  </div>
+</div>
