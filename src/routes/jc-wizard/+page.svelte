62ee6a365eeed2d7760852a29a8d1813cc7a96b2
Lucas Werner <lwerner27@gmail.com>
2024-04-11 18:38:26 +0000

    Added the page/route for the Jamf Connect Wizard.

---
+++ src/routes/jc-wizard/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import LoginGoogle from "../../components/LoginGoogle.svelte";
+    let idp;
+    let app;
+</script>
+
+<div class="w-1/2 mx-auto mt-10">
+
+    <select class="select select-info w-full" bind:value={idp}>
+        <option disabled selected>Select Your IDP</option>
+        <option>Google</option>
+        <option>Azure</option>
+    </select>
+
+    {#if idp === "Google"}
+        <div class="mt-10">
+            <select class="select select-info w-full" bind:value={app}>
+                <option disabled selected
+                    >Select the Application to Configure</option
+                >
+                <option>Jamf Connect Login</option>
+                <option>Jamf Connect Menubar</option>
+            </select>
+        </div>
+    {/if}
+
+    {#if app === "Jamf Connect Login"}
+        <LoginGoogle />
+    {/if}
+
+</div>
