2ac5b809aebc28d8f648cd858fcc4329f4b94761
Lucas Werner <lwerner27@gmail.com>
2024-04-11 18:19:01 +0000

    A component for displaying basic information about the tools.

---
+++ src/components/ToolCard.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let cardColor = "bg-primary";
+    export let cardTitle;
+    export let btnColor = "btn-secondary"
+    export let textColor = "text-neutral"
+    export let btnLink;
+</script>
+
+<div class="card {cardColor} shadow-xl w-96 {textColor}">
+    <div class="card-body">
+        <h2 class="card-title">{cardTitle}</h2>
+        <p><slot /></p>
+        <div class="card-actions justify-end">
+            <a href="{btnLink}">
+                <button class="btn {btnColor}">Try it!</button>
+            </a>
+        </div>
+    </div>
+</div>
