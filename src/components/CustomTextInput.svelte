ae4853f016b1d11e38d8057bb2a3f9674d207dc9
Lucas Werner <lwerner27@gmail.com>
2024-04-11 18:17:21 +0000

    Created a custom text input component for the DaisyUI text input.

---
+++ src/components/CustomTextInput.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let classes;
+    export let placeholder;
+    export let label;
+    export let inputText;
+</script>
+<label class="form-control w-full mb-5">
+    <div class="label">
+        <span class="label-text {classes}">{label}</span>
+    </div>
+    <input
+        bind:value={inputText}
+        type="text"
+        placeholder={placeholder}
+        class="input input-bordered w-full text-neutral-content"
+    />
+</label>
