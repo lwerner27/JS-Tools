c8f222333ed21fbc09b6a01d93b68214f0f54225
Lucas Werner <lwerner27@gmail.com>
2024-04-12 00:23:59 +0000

    Add a name prop, bound to the element & passed in the change handler.

--- src/components/CustomTextInput.svelte
+++ src/components/CustomTextInput.svelte
@@ -2,16 +2,26 @@
     export let classes;
     export let placeholder;
     export let label;
-    export let inputText;
+    export let name;
+    export let handler;
+
+    let element;
+
+    function onInput() {
+        handler(element)
+    }
 </script>
+
 <label class="form-control w-full mb-5">
     <div class="label">
         <span class="label-text {classes}">{label}</span>
     </div>
     <input
-        bind:value={inputText}
+        bind:this={element}
         type="text"
         placeholder={placeholder}
         class="input input-bordered w-full text-neutral-content"
+        name={name}
+        on:input={onInput}
     />
 </label>
